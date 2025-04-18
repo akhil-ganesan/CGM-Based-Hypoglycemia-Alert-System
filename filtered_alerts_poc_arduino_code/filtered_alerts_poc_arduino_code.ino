2d7cb7618dcd62be917c4c753cd860b444db001c
Akhil Ganesan <akhilganesan526@gmail.com>
2025-04-18 16:09:18 -0400

    Tested Gen 3

--- filtered_alerts_poc_arduino_code/filtered_alerts_poc_arduino_code.ino
+++ filtered_alerts_poc_arduino_code/filtered_alerts_poc_arduino_code.ino
@@ -1,5 +1,5 @@
-const float filter_alpha = 0.5
-volatile float prior_in = -1
+const float filter_alpha = 0.5;
+volatile float prior_in = -1;
 const int pinBuzzer = 12; // Buzzer Pin
 volatile float startTime = 0; 
   
@@ -11,22 +11,25 @@ void setup() {
 
 void loop () {
   int time = millis() - startTime;
-  int real_cgm = (30) * (sin(time * 2 * pi() / 3000) + 54 // 3 second repeat period
+  int real_cgm = (30) * (sin(time * 2 * PI / 3000)) + 54; // 3 second repeat period; on for 1 sec & off for 2 sec
   int noise = random(0,30);
-  int in = real_cgm + noise
+  int in = real_cgm + noise;
 
   // Filter if prior > 0; it's negative if first input data point is read
   if (prior_in > 0) {
     int temp = in;
     in = filter(in, prior_in, filter_alpha);
-    prior_in = in;
+    prior_in = temp;
   } else {
-    prior_in = in
+    prior_in = in;
   }
-  Serial.println("Real: " + real_cgm + " Noisy: " + prior_in + "Filtered: " + in);
+  Serial.println("Readings");
+  Serial.println(real_cgm);
+  Serial.println(prior_in);
+  Serial.println(in);
 
   if (in <= 54) {
-    tone(pinBuzzer, 100);
+    tone(pinBuzzer, 10);
   } else {
     noTone(pinBuzzer);
   }
@@ -34,7 +37,7 @@ void loop () {
   delay(1000);
 }
 
-int filter(input, prior, a) {
+int filter(float input, float prior, float a) {
   return input * a + (1 - a) * prior;
 }
 
